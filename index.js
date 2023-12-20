ed69783a9d8761c7c199ac8ba6512f7bff0edbed
Enoch Kibet <enochblake9@gmail.com>
2023-12-20 18:19:26 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+let currentUser = 'John'; 
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+console.log(currentUser);
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
+
